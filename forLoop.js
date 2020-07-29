2e312c748c22ad5e95a98cf7b831e00a19b53b70
jclau69 <jclau69@outlook.com>
2020-07-29 17:37:06 +0200

    adding content quote marks in forLoop.js

--- forLoop.js
+++ forLoop.js
@@ -5,7 +5,7 @@ let content = ' ';
 
 for(let i = 1; i <= 12; i++) {
 
-  content += <img src = photos/thumbnails/${i}.jpg>;
+  content += '<img src = photos/thumbnails/${i}.jpg>';
 }
 
 div.innerHTML = content;
