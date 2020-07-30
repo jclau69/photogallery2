64411c66763d142753d9aaf683edcb260d5d9116
jclau69 <jclau69@outlook.com>
2020-07-30 14:06:43 +0200

    adding loop to project by forLoop.js

--- js/forLoop.js
+++ js/forLoop.js
@@ -6,6 +6,11 @@ let content = ' ';
 for(let i = 1; i <= 12; i++) {
 
 
+  content += `<a href="photos/${i}.jpg" data-caption="I love hay bales. Took this snap on a drive through the countryside past some straw fields.
+  ">
+      <img src="photos/thumbnails/${i}.jpg" alt="Hay Bales">
+  </a>`;
+
 }
 
 div.innerHTML = content;
