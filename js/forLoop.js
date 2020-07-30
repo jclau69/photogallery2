82137b1cc5174f83841e469121432e575fe371d5
jclau69 <jclau69@outlook.com>
2020-07-30 14:21:36 +0200

    adding caption

--- js/forLoop.js
+++ js/forLoop.js
@@ -6,9 +6,9 @@ let content = ' ';
 for(let i = 1; i <= 12; i++) {
 
 
-  content += `<a href="photos/${i}.jpg" data-caption="I love hay bales. Took this snap on a drive through the countryside past some straw fields.
+  content += `<a href="photos/${i}.jpg" data-caption="I love ${photo_captions.txt/${i}.jpg}.
   ">
-      <img src="photos/thumbnails/${i}.jpg" alt="Hay Bales">
+      <img src="photos/thumbnails/${i}.jpg" alt=" ">
   </a>`;
 
 }
