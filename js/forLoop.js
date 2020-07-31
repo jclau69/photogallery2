2f6dd8f3142e486ffdb67148f08f31811a706b3c
jclau69 <jclau69@outlook.com>
2020-07-31 09:42:24 +0200

    adding loop

---
+++ js/forLoop.js
@@ -0,0 +1,16 @@
+
+let div = document.querySelector('div');
+let content = ' ';
+
+
+for(let i = 1; i <= 12; i++) {
+
+
+  content += `<a href="photos/${i}.jpg" data-caption="I love ...
+  ">
+      <img src="photos/thumbnails/${i}.jpg" alt=" ">
+  </a>`;
+
+}
+
+div.innerHTML = content;
