0734de1f3891112f2dcb1709e98dffc2b6e167cb
jclau69 <jclau69@outlook.com>
2020-08-03 14:41:40 +0200

    moving script forLoop

--- js/forLoop.js
+++ js/forLoop.js
@@ -1,12 +1,22 @@
 
 let div = document.querySelector('div');
 let content = ' ';
+let arrayText = 'photo_captions.txt';
 
+function insertTextPic(arr){
+
+let items = '';
+for(let i = 0; i <= arrayText.length; i++) {
+
+items += `${arr.length}`;
+}
+return items;
+}
 
 for(let i = 1; i <= 12; i++) {
 
 
-  content += `<a href="photos/${i}.jpg" data-caption="I love ...
+  content += `<a href="photos/${i}.jpg" data-caption="I love ${insertTextPic(arrayText)} 
   ">
       <img src="photos/thumbnails/${i}.jpg" alt=" ">
   </a>`;
